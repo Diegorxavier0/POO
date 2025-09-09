30315cb4a5c7c1edccb2cbe71b3b64488afc56c7
Diego Rodrigues Xavier <diegorodriguesxavier970@gmail.com>
2025-09-09 19:17:09 -0300

    criação da pasta 02_Construtores

--- 02_Construtores/Program.cs
+++ 02_Construtores/Program.cs
@@ -20,11 +20,24 @@ namespace _02_Contrutores
 
         {
 
-            Quadrado quadrado = new Quadrado(5); //
+            Quadrado quadrado = new Quadrado(); //
 
             quadrado.ImprimirArea();
 
+            Quadrado quadrado1 = new Quadrado(10); //
+
+            quadrado1.ImprimirArea();
+
+            Retangulo retangulo = new Retangulo(7, 8);
+
+            retangulo.ImprimirArea();
+
+            Triangulo triangulo = new Triangulo(9, 10);
+
+            triangulo.ImprimirArea();
+
         }
+
 
     }
 
@@ -37,6 +50,18 @@ namespace _02_Contrutores
         //pode ser usado apenas dentro da Classe
 
         private int Lado; //Atributo
+
+        //Overload (Sobrecarga) de Cosnstrutor
+
+        //Declarar mais de um construtor com a lista de argumentos diferentes
+
+        public Quadrado()
+
+        {
+
+            this.Lado = 5;
+
+        }
 
         public Quadrado(int Lado) //declaração do construtor (nunca tem retorno, ele  serve para instanciar um objeto e inicializar os atributos 
 
@@ -61,6 +86,74 @@ namespace _02_Contrutores
         {
 
             Console.WriteLine($"Quadrado com lado de {Lado} possui uma área de {CalculaArea()}");
+
+        }
+
+    }
+
+    public class Retangulo
+
+    {
+
+        private int Base, Altura;
+
+        public Retangulo(int Base, int Altura)
+
+        {
+
+            this.Altura = Altura;
+
+            this.Base = Base;
+
+        }
+
+        public int CalculaArea()
+
+        {
+
+            return Base * Altura;
+
+        }
+
+        public void ImprimirArea()
+
+        {
+
+            Console.WriteLine($"Retangulo com lado de {Base} e altura de {Altura} possui uma área de {CalculaArea()}");
+
+        }
+
+    }
+
+    public class Triangulo
+
+    {
+
+        private int Base, Altura;
+
+        public Triangulo(int Base, int Altura)
+
+        {
+
+            this.Altura = Altura;
+
+            this.Base = Base;
+
+        }
+
+        public int CalculaArea()
+
+        {
+
+            return (Base * Altura) / 2;
+
+        }
+
+        public void ImprimirArea()
+
+        {
+
+            Console.WriteLine($"Triangulo com lado de {Base} e altura de {Altura} possui uma área de {CalculaArea()}");
 
         }
 
