cffa05ef32667c90230a977f9b03262430275efa
Diego Rodrigues Xavier <diegorodriguesxavier970@gmail.com>
2025-09-12 21:48:19 -0300

    Criando um calendario atraves de uma classe

---
+++ 06_Calendario/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06_Calendario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o ano: ");
+            int ano = int.Parse(Console.ReadLine());
+
+            Calendario calendario;
+           
+            for (int mes = 1; mes <= 12; mes++)
+            {
+                calendario = new Calendario(ano, mes);
+                calendario.ImprimeCalendario();
+                Console.WriteLine("\n\n");
+
+            }
+            Console.ReadKey();
+        }
+
+    
+    }
+}
