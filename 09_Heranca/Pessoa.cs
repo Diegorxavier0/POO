13d087d235d9aa2c638dc629862377562995bc79
Diego Rodrigues Xavier <diegorodriguesxavier970@gmail.com>
2025-09-18 22:20:51 -0300

    Herança

---
+++ 09_Heranca/Pessoa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Heranca
+{
+    internal abstract class Pessoa
+    {
+        public string Nome;
+        public string Telefone;
+
+        public abstract string GetDocumento();
+
+        public void Imprimir()
+        {
+            Console.WriteLine($"\nLISTA DE PESSOAS\n\nNome: {this.Nome} Documento {this.GetDocumento()} telefone: {this.Telefone}");
+
+        }
+    }
+}    
+
---
+++ 09_Heranca/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Heranca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Nao é possivel instanciar uma classe abstrata
+
+            //Testa o pessoa fisica
+            PessoaFisica pessoaFisica = new PessoaFisica();
+            pessoaFisica.Nome = "Geraldo";
+            pessoaFisica.CPF = "123.456.789-00";
+            pessoaFisica.Telefone = "(11) 91234-5678";
+            pessoaFisica.Imprimir();
+
+            //Testa o pessoa juridica
+            PessoaJuridica pessoaJuridica = new PessoaJuridica();
+            pessoaJuridica.Nome = "Paletas Mexicanas";
+            pessoaJuridica.CNPJ = "12.345.678/0001-00";
+            pessoaJuridica.Telefone = "(014) 9875-4321";
+            pessoaJuridica.Imprimir();
+
+
+        }
+    }
+}
