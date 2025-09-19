cb8cee47b41881b5f8757cfe46814c743cc9a9e3
Diego Rodrigues Xavier <diegorodriguesxavier970@gmail.com>
2025-09-19 20:43:09 -0300

    Polimorfismo e membros da classe object

--- 09_Heranca/Pessoa.cs
+++ 09_Heranca/Pessoa.cs
@@ -8,8 +8,10 @@ namespace _09_Heranca
 {
     internal abstract class Pessoa
     {
+        public int Id { get; set; }
         public string Nome;
         public string Telefone;
+
 
         public abstract string GetDocumento();
 
@@ -17,6 +19,27 @@ namespace _09_Heranca
         {
             Console.WriteLine($"\nLISTA DE PESSOAS\n\nNome: {this.Nome} Documento {this.GetDocumento()} telefone: {this.Telefone}");
 
+        }
+
+        public override string ToString() { 
+         
+            return $"<toString()> Nome: {this.Nome} Documento {this.GetDocumento()} telefone: {this.Telefone}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null /*|| this.GetType() != obj.GetType()*/) 
+                return false;
+
+            //Cast=> conversao de objeto
+            // ((Pessoa)obj).Id=> converte obj que é do tipo Object para o tipo pessoa
+
+            return this.Id == ((Pessoa)obj).Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Id.GetHashCode();   
         }
     }
 }    
--- 09_Heranca/Program.cs
+++ 09_Heranca/Program.cs
@@ -12,8 +12,14 @@ namespace _09_Heranca
         {
             //Nao é possivel instanciar uma classe abstrata
 
+
             //Testa o pessoa fisica
+
+            Pessoa[] pessoas = new Pessoa[2];//Criação do vetor
+
             PessoaFisica pessoaFisica = new PessoaFisica();
+            pessoas[0] = pessoaFisica;
+            pessoaFisica.Id = 1;
             pessoaFisica.Nome = "Geraldo";
             pessoaFisica.CPF = "123.456.789-00";
             pessoaFisica.Telefone = "(11) 91234-5678";
@@ -21,12 +27,29 @@ namespace _09_Heranca
 
             //Testa o pessoa juridica
             PessoaJuridica pessoaJuridica = new PessoaJuridica();
+            pessoas[1] = pessoaJuridica;
+            pessoaJuridica.Id = 1;
             pessoaJuridica.Nome = "Paletas Mexicanas";
             pessoaJuridica.CNPJ = "12.345.678/0001-00";
             pessoaJuridica.Telefone = "(014) 9875-4321";
             pessoaJuridica.Imprimir();
 
+            Console.WriteLine(pessoaFisica);
+            Console.WriteLine(pessoaJuridica);
 
+            if (pessoaFisica.Equals(pessoaJuridica))
+            {
+                Console.WriteLine("\nÉ a mesma pessoa");
+            }
+            else
+            {
+                Console.WriteLine("Não é a mesma pessoa");
+            }
+
+            foreach (Pessoa pessoa in pessoas)
+            {
+                Console.WriteLine(pessoa);
+            }
         }
     }
 }
