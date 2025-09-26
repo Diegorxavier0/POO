560693922e4df5f2877e06f4d6c31dc0844ce8c2
Diego Rodrigues Xavier <diegorodriguesxavier970@gmail.com>
2025-09-25 22:23:54 -0300

    13_ColecaoPilha

---
+++ 13_ColecaoPilha/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq; // precisa para usar Reverse()
+
+namespace _13_ColecaoPilha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> pilha = new Stack<int>();
+            pilha.Push(1);
+            pilha.Push(2);
+            pilha.Push(3);
+            pilha.Push(4);
+            pilha.Push(5);
+
+            Console.WriteLine("Elementos na pilha (ordem de entrada):");
+            foreach (var item in pilha.Reverse())
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("\nImpressão dos pilhaNomes\n");
+            Console.WriteLine($"Ultimo inserido: {pilha.Pop()}");
+        }
+    }
+}
