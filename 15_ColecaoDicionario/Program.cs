5967f0f1c47f85166ee5349818e00b48418d9384
Diego Rodrigues Xavier <diegorodriguesxavier970@gmail.com>
2025-09-29 20:25:12 -0300

    Enum

--- 15_ColecaoDicionario/Program.cs
+++ 15_ColecaoDicionario/Program.cs
@@ -1,45 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
 
-namespace _15_ColecaoDicionario
+enum DiaDaSemana
 {
-    internal class Program
+    Segunda,
+    Terca,
+    Quarta,
+    Quinta,
+    Sexta,
+    Sabado,
+    Domingo
+}
+
+class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        DiaDaSemana hoje = DiaDaSemana.Terca;
+
+        if (hoje == DiaDaSemana.Terca)
         {
-            Dictionary<int, string> dicAlunos = new Dictionary<int, string>();
-
-            Console.Write("\nCadastre três alunos: \n ");
-            //int qtd = int.Parse(Console.ReadLine());
-
-           for (int i = 0; i < 3; i++)//se quiser que deixe pro usuario digitar a quantidade, ao inves de 3, altere para qtd,
-                                      //e descomente a linha 12
-            {
-                Console.Write("\nDigite o RA: ");
-                int ra = int.Parse(Console.ReadLine());
-
-                string nome = "";
-                while (nome.Length < 3)
-                {
-                    Console.Write("Digite o nome: ");
-                    nome = Console.ReadLine();
-
-                    if (nome.Length < 3)
-                    {
-                        Console.WriteLine("Nome precisa ter pelo menos 3 letras!");
-                    }
-                }
-
-                dicAlunos.Add(ra, nome);
-            }
-
-            Console.WriteLine("\nAlunos cadastrados:");
-            foreach (var aluno in dicAlunos)
-            {
-                Console.WriteLine("RA: " + aluno.Key + " - Nome: " + aluno.Value);
-            }
-
-            Console.ReadKey();
+            Console.WriteLine($"Hoje é {DiaDaSemana.Terca}");
         }
     }
 }
