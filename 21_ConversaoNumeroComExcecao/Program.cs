ac2d26b07b85359322970f54ec197ab5648b2605
Diego Rodrigues Xavier <diegorodriguesxavier970@gmail.com>
2025-10-09 19:56:26 -0300

    21_ConversaoNumeroComExcecao

---
+++ 21_ConversaoNumeroComExcecao/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _21_ConversaoNumeroComExcecao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número inteiro: ");
+            string valorDigitado = Console.ReadLine();
+
+            try
+            {
+                int numero = Convert.ToInt32(valorDigitado);
+                int dobro = numero * 2;
+                Console.WriteLine($"O dobro de {numero} é {dobro}.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Valor inválido! Digite apenas números inteiros.");
+            }
+
+            Console.WriteLine("\nPressione qualquer tecla para sair...");
+            Console.ReadKey();
+        }
+    }
+}
